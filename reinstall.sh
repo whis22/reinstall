93db2e6443b4cd24fff090184d090717ff372d6d
whis22 <776635420@qq.com>
2025-11-29 21:28:22 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,9 +3,8 @@
 # shellcheck disable=SC2086
 
 set -eE
-confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-confhome_cn=https://cnb.cool/bin456789/reinstall/-/git/raw/main
-# confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
+confhome=https://raw.githubusercontent.com/whis22/reinstall/main
+confhome_cn=https://ghproxy.it/https://raw.githubusercontent.com/whis22/reinstall/main
 
 # 用于判断 reinstall.sh 和 trans.sh 是否兼容
 SCRIPT_VERSION=4BACD833-A585-23BA-6CBB-9AA4E08E0004
